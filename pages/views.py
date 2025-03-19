89c3713809ed07c504084cd8ba4d06fb71266f85
Vonda <lavondedunigan05@gmail.com>
2025-03-18 19:25:21 -0500

    corrected

---
+++ pages/views.py
@@ -0,0 +1,15 @@
+from django.views.generic import TemplateView
+
+
+class HomePageView(TemplateView):
+    template_name = "pages/home.html"
+    
+    
+class AboutPageView(TemplateView):
+    template_name = "pages/about.html"
+    
+    
+    
+    
+    
+    
